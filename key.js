832e0ea249b850bc00d323c5e92db0c86c0ac0ac
Jessica Miles <Jessica Miles>
2017-04-04 21:47:49 -0400

    fingers crossed this worked

--- key.js
+++
@@ -1,9 +0,0 @@
-
-//console.log('Key.js is loaded');
-//Twitter Key for my account
-exports.twitterFeed = {
-  consumer_key: 'cUtrfNztR4UAuYhBzvWTCzYVa',
-  consumer_secret: 	'VX0cogdd0sRjD0I6VK9ES46ngkhsuyy8G4hxBAMR00OxbVZlrJ',
-  access_token_key: '2978183356-ymQDnuHplIBHI4iOy8MCRdGR4PPVioi2ZqUyf5i',
-  access_token_secret: 'J8rD9527IdEtdBfBkNqpcfuGJcZ73XgTL05ySBJs9u3Yf',
-}
